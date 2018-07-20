2bbadb9852b61a629ad774c63b1546e6212fca7e
Claire Obrien <claireobrien@Claires-MacBook-Pro.local>
2018-07-20 13:19:55 -0600

    made the random word function

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
+
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <p> {{word}} </p>
   </div>
 </template>
 
 <script>
+
+import unirest from 'unirest';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'New Hangman Game'
+      msg: 'New Hangman Game',
+      wordBank: ['Hangman', 'Detroit', 'Happiness','Kite','River'],
+      index: 0,
+      word: ''
     }
-  }
+  },
+  created() {
+      this.randomWordApi()
+  },
+  methods: {
+    getRandomWord() {
+     let index = Math.floor((Math.random() * this.wordBank.length + 1))
+
+     let word = this.wordBank[index]
+
+     this.wordBank.splice(index, 1)
+
+     return word
+   },
+    async randomWordApi(){
+      fetch('https://wordsapiv1.p.mashape.com/words?random=true', {
+        headers: {
+          "X-Mashape-Key": "tW0afqvn72mshW26R5qSzD0Ix8g5p19lwqcjsnGKh4XVnyKcHW",
+          "Accept": "application/json"
+        }
+      }).then(res => res.json())
+      .then((data) => {this.word = data.word})
+    }   
 }
 </script>
 
