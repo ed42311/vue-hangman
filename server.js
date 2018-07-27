7e692355b697ea6cf78dbfa4fb31b0a359f47a95
Claire Obrien <claireobrien@Claires-MacBook-Pro.local>
2018-07-27 09:33:40 -0600

    fixing errors

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var app = express();
 
 var port = process.env.PORT || 3000
 
-app.use("/", express.static(path.join(__dirname,'/')));
+app.use("/", express.static(path.join(__dirname,'/public')));
 
 app.listen(port, () => {console.log(`app running on ${port}`)});
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
     output: {
       path: path.join(__dirname),
-      filename: './dist/build.js',
+      filename: './public/dist/build.js',
       publicPath: '/'
     },
   module: {
